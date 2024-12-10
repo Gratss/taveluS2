8154579a91067b5278edbd974293aee39c9bae5f
Gratss <snaelzzz@gmail.com>
2024-12-11 01:37:03 +0300

    + функции3

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Post, Body, Param } from '@nestjs/common';
+import { Controller, Get, Post, Put, Delete, Param, Body } from '@nestjs/common';
 import { UserService } from './user.service';
 import { User } from './user.entity';
 
@@ -6,22 +6,54 @@ import { User } from './user.entity';
 export class UserController {
   constructor(private readonly userService: UserService) {}
 
-  // создание 
+
   @Post()
   async createUser(@Body() userData: { name: string; email: string; password: string }) {
-    const { name, email, password } = userData;
-    return await this.userService.createUser(name, email, password);
+    return await this.userService.createUser(userData.name, userData.email, userData.password);
   }
 
-  // поиск всех 
+
   @Get()
   async findAll(): Promise<User[]> {
     return await this.userService.findAll();
   }
 
-  // получение 1ого 
+  // получение 1ого юзара по айди
   @Get(':id')
   async findOne(@Param('id') id: number): Promise<User> {
     return await this.userService.findOne(id);
+  }
+
+  // обнова данных
+  @Put(':id')
+  async updateUser(
+    @Param('id') id: number,
+    @Body() updateData: Partial<{ name: string; email: string; password: string }>,
+  ): Promise<User> {
+    return await this.userService.updateUser(id, updateData);
+  }
+
+  // удалить юзера
+  @Delete(':id')
+  async deleteUser(@Param('id') id: number): Promise<void> {
+    return await this.userService.deleteUser(id);
+  }
+
+  // find po emailu
+  @Get('email/:email')
+  async findByEmail(@Param('email') email: string): Promise<User> {
+    return await this.userService.findByEmail(email);
+  }
+
+  // блокнуть
+  @Put(':id/block')
+  async blockUser(@Param('id') id: number): Promise<User> {
+    return await this.userService.blockUser(id);
+  }
+
+  @Get('verify-email/:token')
+  async verifyEmail(@Param('token') token: string) {
+    await this.userService.verifyEmail(token);
+    return { message: 'Email successfully verified!' };
   }
 }
